88bece11f351df8917ba60bb010e515fe9217866
ddekker@mills.edu <ddekker@mills.edu>
2017-04-10 11:56:47 -0700

    take out android logo

--- app/src/main/java/edu/mills/findeatfood/MainActivity.java
+++ app/src/main/java/edu/mills/findeatfood/MainActivity.java
@@ -71,13 +71,16 @@ public class MainActivity extends Activity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = getString(R.string.find_recipes);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = getString(R.string.find_food_deals);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = getString(R.string.about);
+                break;
+            case 4:
+                mTitle = getString(R.string.favorites);
                 break;
         }
     }
