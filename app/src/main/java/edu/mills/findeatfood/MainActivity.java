ccf06299d26a92fe12d6b325f3b3afee3c6fc963
ddekker@mills.edu <ddekker@mills.edu>
2017-04-10 18:17:38 -0700

    change mainactivity

--- app/src/main/java/edu/mills/findeatfood/MainActivity.java
+++ app/src/main/java/edu/mills/findeatfood/MainActivity.java
@@ -1,135 +1,217 @@
 package edu.mills.findeatfood;
 
 import android.app.Activity;
-
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.os.Build;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+import java.util.List;
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+public class MainActivity extends Activity {
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+    private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private int currentPosition = 0;
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            //call when item in the drawer is clicked
+            selectItem(position);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        //reference to Drawer Layout
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //Populate the ListView
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt("position");
+            setActionBarTitle(currentPosition);
+        }else{
+            selectItem(0);
+        }
+        drawerToggle = new DrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(drawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        getFragmentManager().addOnBackStackChangedListener(new FragManager());
+    }
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+    private class FragManager implements FragmentManager.OnBackStackChangedListener{
+
+        public void onBackStackChanged(){
+            FragmentManager fragMan = getFragmentManager();
+            Fragment fragment = fragMan.findFragmentByTag("visible_fragment");
+            if (fragment instanceof TopFragment) {
+                currentPosition = 0;
+            }
+            if (fragment instanceof PizzaFragment) {
+                currentPosition = 1;
+            }
+            if (fragment instanceof PastaFragment) {
+                currentPosition = 2;
+            }
+            if (fragment instanceof StoresFragment) {
+                currentPosition = 3;
+            }
+            setActionBarTitle(currentPosition);
+            drawerList.setItemChecked(currentPosition, true);
+
+        }
+    }
+
+
+    private class DrawerToggle extends ActionBarDrawerToggle {
+
+        DrawerToggle(Activity activity,
+                     DrawerLayout drawerLayout,
+                     int openDrawerContentDescRes,
+                     int closeDrawerContentDescRes) {
+            super(activity, drawerLayout, openDrawerContentDescRes, closeDrawerContentDescRes);
+
+        }
+
+        //Called when a drawer has settled in a completely closed state
+        @Override
+        public void onDrawerClosed(View view) {
+            super.onDrawerClosed(view);
+            invalidateOptionsMenu();
+        }
+
+        //Called when a drawer has settled in a completely open state.
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            invalidateOptionsMenu();
+        }
+    }
+
+
+    private void selectItem(int position){
+        Fragment fragment;
+        currentPosition = position;
+        switch(position){
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment, "visible_fragment");
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+        //set action bar title
+        setActionBarTitle(position);
+        //close drawer
+        drawerLayout.closeDrawer(drawerList);
     }
 
     @Override
-    public void onNavigationDrawerItemSelected(int position) {
-//        // update the main content by replacing fragments
-//        FragmentManager fragmentManager = getFragmentManager();
-//        fragmentManager.beginTransaction()
-//                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-//                .commit();
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the drawer is open, hide action items related to the content view
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
 
-        // rudimentarily call activities for now
-        switch (position) {
-            case 1:
-                Intent intent = new Intent(MainActivity.this, IngredientActivity.class);
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("position", currentPosition);
+    }
+
+    private void setActionBarTitle(int position){
+        String title;
+        if(position == 0){
+            title = getResources().getString(R.string.app_name);
+        }else{
+            title = titles[position];
+        }
+        getActionBar().setTitle(title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        setIntent("This is example text");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                //Code to run when the Create Order item is clicked
+                Intent intent = new Intent(this, OrderActivity.class);
                 startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                //Code to run when the settings item is clicked
                 break;
-            case 2:
-                intent = new Intent(MainActivity.this, FavoritesActivity.class);
-                startActivity(intent);
-                break;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+        return true;
     }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.find_recipes);
-                break;
-            case 2:
-                mTitle = getString(R.string.find_food_deals);
-                break;
-            case 3:
-                mTitle = getString(R.string.about);
-                break;
-            case 4:
-                mTitle = getString(R.string.favorites);
-                break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
---
+++ app/src/main/java/edu/mills/findeatfood/OrderActivity.java
@@ -0,0 +1,16 @@
+package edu.mills.findeatfood;
+
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
