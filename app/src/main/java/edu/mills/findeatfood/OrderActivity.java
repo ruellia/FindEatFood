b686f3ef8a110a153a9f129a9d03d6a547774443
ddekker@mills.edu <ddekker@mills.edu>
2017-04-12 14:31:33 -0700

    add OrderActivity

---
+++ app/src/main/java/edu/mills/findeatfood/OrderActivity.java
@@ -0,0 +1,18 @@
+package edu.mills.findeatfood;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
