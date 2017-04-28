10aab9932b14ca3bdea31d1a818c5dc1d778ae20
Ruellia Zhang <syncyes@gmail.com>
2017-04-27 21:20:40 -0700

    http util for yummly search recipes and refactor http util class

---
+++ app/src/main/java/edu/mills/findeatfood/models/Matches.java
@@ -0,0 +1,11 @@
+package edu.mills.findeatfood.models;
+
+public class Matches {
+    public final String recipeName;
+    public final String id;
+
+    public Matches(String recipeName, String id) {
+        this.recipeName = recipeName;
+        this.id = id;
+    }
+}
