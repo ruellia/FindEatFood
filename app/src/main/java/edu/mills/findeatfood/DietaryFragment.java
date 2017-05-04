dab9f691f8dba050ab81af4d42144acaff820218
Gloriane Tran <glorianetran@gmail.com>
2017-05-04 12:35:26 -0700

    javadoc for dietaryFragment

--- app/src/main/java/edu/mills/findeatfood/DietaryFragment.java
+++ app/src/main/java/edu/mills/findeatfood/DietaryFragment.java
@@ -22,43 +22,27 @@ public class DietaryFragment extends Fragment {
 
     private List<Integer> dietOptionsIds = new ArrayList<Integer>();
     private List<Integer> allergyOptionsIds = new ArrayList<Integer>();
-    OnDietOptionsSelectedListener dietOptionsSelectedListener;
+    private OnDietOptionsSelectedListener dietOptionsSelectedListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dietary, container, false);
-
         String[] dietOptions =
                 getResources().getStringArray(R.array.dietOptionsName);
         String[] allergyOptions =
                 getResources().getStringArray(R.array.allergyOptionsName);
-
         addCheckBoxes(dietOptions, view, dietOptionsIds);
         addCheckBoxes(allergyOptions, view, allergyOptionsIds);
 
+        //calls interface method for passing data to MainActivity
         dietOptionsSelectedListener.onDietOptionsSelected(dietOptionsIds, allergyOptionsIds);
         return view;
-    }
-
-    public void addCheckBoxes(String[] options,
-                              View view,
-                              List<Integer> optionsIds ){
-        LinearLayout dietaryOptionsLayout =
-                (LinearLayout) view.findViewById(R.id.dietaryOptionsLayout);
-        for (int i = 0; i < options.length; i++) {
-        CheckBox checkBox = new CheckBox(this.getActivity());
-        checkBox.setText(options[i]);
-        checkBox.setTextColor(getActivity().getResources().getColor(R.color.white));
-        int checkBoxId = view.generateViewId();
-        optionsIds.add(checkBoxId);
-        checkBox.setId(checkBoxId);
-        dietaryOptionsLayout.addView(checkBox);
-        }
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        //makes sure class is implementing interface
         try {
             dietOptionsSelectedListener = (OnDietOptionsSelectedListener) activity;
         } catch (ClassCastException e) {
@@ -66,7 +50,36 @@ public class DietaryFragment extends Fragment {
         }
     }
 
+    private void addCheckBoxes(String[] options,
+                               View view,
+                               List<Integer> optionsIds) {
+        LinearLayout dietaryOptionsLayout =
+                (LinearLayout) view.findViewById(R.id.dietaryOptionsLayout);
+
+        for (int i = 0; i < options.length; i++) {
+            CheckBox checkBox = new CheckBox(this.getActivity());
+            checkBox.setText(options[i]);
+            checkBox.setTextColor(getActivity().getResources().getColor(R.color.white));
+            int checkBoxId = view.generateViewId();
+            optionsIds.add(checkBoxId);
+            checkBox.setId(checkBoxId);
+            dietaryOptionsLayout.addView(checkBox);
+        }
+    }
+
+    /**
+     * Used for transferring ArrayList of ids for
+     * dietary and allergy options into
+     * {@code MainActivity}.
+     */
     public interface OnDietOptionsSelectedListener {
+        /**
+         * Passes ArrayLists of dietary and allergy options ids
+         * into MainActivity.
+         *
+         * @param dietOptionsIds    ArrayList of ids for dietary options.
+         * @param allergyOptionsIds ArrayList of ids for allergy options.
+         */
         void onDietOptionsSelected(List<Integer> dietOptionsIds, List<Integer> allergyOptionsIds);
     }
 }
