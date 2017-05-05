be7e8ca47cbcbe24210011b53bcff226d610dd63
Angelica <aleyvamcmurtry@mills.edu>
2017-05-05 12:12:39 -0700

    Fixed java doc in RecipeDatabaseUtilities and in SQLiteRecipeOpenHelper. Removed recipes inserted in SQLiteRecipeOpenHelper and fixed the test for it.

--- app/src/androidTest/java/edu/mills/findeatfood/RecipeDatabaseUtilitiesTest.java
+++ app/src/androidTest/java/edu/mills/findeatfood/RecipeDatabaseUtilitiesTest.java
@@ -8,10 +8,7 @@ import android.test.RenamingDelegatingContext;
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.mills.findeatfood.models.Recipe;
-
 import static org.junit.Assert.assertEquals;
-
 
 public class RecipeDatabaseUtilitiesTest {
 
@@ -21,18 +18,22 @@ public class RecipeDatabaseUtilitiesTest {
 
     @Before
     public void setUp() throws Exception {
-        //This creates a temporary context so database accesses in the tests are isolated.
+        // This creates a temporary context so database accesses in the tests are isolated.
         RenamingDelegatingContext context =
                 new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(), "_test");
         db = new SQLiteRecipeOpenHelper(context).getWritableDatabase();
+
+        // Entering mock data to test RecipeDatabaseUtilities methods.
+        RecipeDatabaseUtilities.insertRecipe(db, "Beef Bourguignon", "Beef-bourguignon-333851");
+        RecipeDatabaseUtilities.insertRecipe(db, "Pork Chops Au Poivre", "Pork-Chops-Au-Poivre-1568332");
     }
 
     @Test
     public void getRecipes() throws Exception {
-        // gets all the recipes from the database
+        // Gets all the recipes from the database.
         Cursor recipeCursor = RecipeDatabaseUtilities.getAllRecipes(db);
 
-        // verifies all recipes were returned and correct
+        // Verifies all recipes were returned and correct.
         int j = 0;
         while (recipeCursor.moveToNext()) {
             String recipeName = recipeCursor.getString(0);
@@ -45,17 +46,17 @@ public class RecipeDatabaseUtilitiesTest {
 
     @Test
     public void insertRecipes() throws Exception {
-        // inserts recipe into database
+        // Inserts recipe into database.
         RecipeDatabaseUtilities.insertRecipe(db, "CHICKEN SOUP", "Chicken-Soup-000000");
 
-        // get cursor to all recipes
+        // Get cursor to all recipes.
         Cursor recipeCursor = RecipeDatabaseUtilities.getAllRecipes(db);
 
-        // move past the default recipes
+        // Move past the default recipes.
         recipeCursor.moveToNext();
         recipeCursor.moveToNext();
 
-        // verify that recipe was inserted properly
+        // Verify that recipe was inserted properly.
         recipeCursor.moveToNext();
         String recipeName = recipeCursor.getString(0);
         String recipeId = recipeCursor.getString(1);
@@ -64,28 +65,28 @@ public class RecipeDatabaseUtilitiesTest {
     }
 
     @Test
-    public void searchForRecipe() throws Exception{
-        // checks if recipe is in db
+    public void searchForRecipe() throws Exception {
+        // Checks if recipe is in db.
         Boolean isRecipe = RecipeDatabaseUtilities.searchForRecipe(db, "Beef-bourguignon-333851");
 
-        // verifies recipe is in db
+        // Verifies recipe is in db.
         assertEquals(true, isRecipe);
 
-        // checks if recipe is in db
+        // Checks if recipe is in db.
         isRecipe = RecipeDatabaseUtilities.searchForRecipe(db, "Carrot-soup-827394");
 
-        //verifies recipe is not in db
+        // Verifies recipe is not in db.
         assertEquals(false, isRecipe);
     }
 
     @Test
-    public void deleteRecipe() throws Exception{
-        // deletes recipe from database
-        RecipeDatabaseUtilities.deleteRecipe(db,"Beef-bourguignon-333851");
+    public void deleteRecipe() throws Exception {
+        // Deletes recipe from database.
+        RecipeDatabaseUtilities.deleteRecipe(db, "Beef-bourguignon-333851");
 
-        // checks if recipe was deleted
+        // Checks if recipe was deleted.SS
         Boolean isRecipe =
-                RecipeDatabaseUtilities.searchForRecipe(db,"Beef-bourguignon-333851");
+                RecipeDatabaseUtilities.searchForRecipe(db, "Beef-bourguignon-333851");
         assertEquals(false, isRecipe);
     }
 }
