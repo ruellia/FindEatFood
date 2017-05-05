9d593c5e3081dfdc3575f764506ef2e424c6ec97
Ruellia Zhang <syncyes@gmail.com>
2017-05-05 14:24:33 -0700

    modify models package info

--- app/src/main/java/edu/mills/findeatfood/models/package-info.java
+++ app/src/main/java/edu/mills/findeatfood/models/package-info.java
@@ -1,4 +1,6 @@
 /**
- * Models for parsing JSON into Java objects.
+ * Models for parsing JSON into Java objects with Moshi.
+ * See <a href="https://github.com/square/moshi">Moshi</>
+ * and its <a href="https://github.com/square/moshi/tree/master/examples/src/main/java/com/squareup/moshi/recipes/models">model examples</a>.
  */
 package edu.mills.findeatfood.models;
