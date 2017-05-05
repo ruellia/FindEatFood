86fb1bbbacca809c61b5b6549a1593b5030a8801
deanabui <deanabui@gmail.com>
2017-05-05 13:08:39 -0700

    added javadoc to IngredientsFragment

--- app/src/main/java/edu/mills/findeatfood/IngredientsFragment.java
+++ app/src/main/java/edu/mills/findeatfood/IngredientsFragment.java
@@ -6,6 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+/**
+ * Displays an editText and button. Clicking the next button will parse the string from the
+ * editText into a string array, add the ingredients to the recipe API call, and launch
+ * {@link DietaryFragment}.
+ */
 public class IngredientsFragment extends Fragment {
 
     @Override
